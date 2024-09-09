fb7a1f61f32c71ebb8c2181311f5f02fd0a9f7e9
Adjibousso <diallobousso10@gmail.com>
2024-09-08 20:52:43 -0400

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,14 @@
 const users = [
-    { id: 1, name: "Alice" },
-    { id: 2, name: "Bob" },
-    { id: 3, name: "Charlie" },
-    { id: 4, name: "David" },
-    { id: 5, name: "Eve" },
-    { id: 6, name: "Frank" },
-    { id: 7, name: "Grace" },
-    { id: 8, name: "Hannah" },
-    { id: 9, name: "Ivy" },
-    { id: 10, name: "Jack" }
+    { id: 1, name: "Alice" ,address: "156 colerain ave ,Cincinnati"},
+    { id: 2, name: "Bob", address: "16 montana ave ,Texas" },
+    { id: 3, name: "Charlie" ,address: "34 Martin luther king blvd ,Washington"},
+    { id: 4, name: "David" ,address: "37 Baobab ave ,Dakar"},
+    { id: 5, name: "Eve",address: "81 Sandaga ,senegal" },
+    { id: 6, name: "Frank",address: "196 Lamine Gueye ave ,Dakar" },
+    { id: 7, name: "Grace" ,address: "4th  ,New York"},
+    { id: 8, name: "Hannah",address: "6 Bulls ave ,CHicago" },
+    { id: 9, name: "Ivy" ,address: "120 colspan ave ,Colorado"},
+    { id: 10, name: "Jack" ,address: "100 mille colline  ave ,Afrique"}
   ];
 
 
--- server.js
+++ server.js
@@ -1,22 +1,76 @@
 const express = require("express");
 const app = express ();
 const port = 3000;  
-let users = require('./routes/users')
-app.listen(port, () => {
+const morgan = require('morgan');
+const helper= require('./helper.js');
+const routes = require('./routes/routindex.js');
+let users = require('./routes/users');
 
-    console.log("server star", port) 
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.get('/', function(req,res){
+    var obj = {
+         name:"Bousso",
+         age : "90 ans"}
+    res.render("index" , obj);
 })
-    app.get("/" ,( req, res) =>{
-        res.send('ibrahima and adji love you ')
-    } )
 
-    app.get('/users/name/:id', (req, res)=> {
-        const id = parseInt(req.params.id)
-        const user = users.find (user => user.id===id)
 
-        res.send(`l'identite de l'utilisateur est le numero:  ${user.name}`)
-    })
 
-    app.get('/users', (req, res)=> {
-        res.send(`the number of users is ${users.length } `)
-    })
+
+
+
+
+
+// app.use((req, res, next) => {
+//     console.log(`URL: ${req.url}`)
+//     next()
+
+// })
+
+// app.use ('/api/v1/routes' ,routes)
+
+
+// app.use(morgan('dev'))
+
+
+ app.listen(port, () => {
+
+  console.log("server star", port) 
+})
+// //routes
+//     app.get("/" ,( req, res) =>{
+//         res.send('ibrahima 2 ')
+//     } )
+
+//     app.get('/users/:id', (req, res)=> {
+//         const id = parseInt(req.params.id)
+//         const user = users.find (user => user.id===id)
+//         const message= "I found you "
+//         res.json(helper.success(message,user))
+
+//         // res.send(`l'identite de l'utilisateur est le numero:  ${user.name}`)
+        
+//     })
+
+
+    // app.get('/users', (req, res)=> {
+    //     res.send(`the number of users is ${users.length } `)
+        
+    // })
+
+    // app.get('/users', (req, res)=>{ 
+    //     const message = 'the number of users is '
+    //     res.json(helper.success(message,users))
+    // })
+    // app.post('/users',(req,res) =>{
+    //     const id = 11
+    //     const userNew = { ...req,body,...(id = id, created= new Date())}
+    // users.push(userNew)
+    // const message= `the user ${userNew.name} has been created`
+    // res.json(helper.success(message,use
+
+    // ))
+    // })
+    
