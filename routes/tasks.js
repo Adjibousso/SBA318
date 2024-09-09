3e66070d791df6e91d3fd609c8c5f7892f6fa2e0
Adjibousso <diallobousso10@gmail.com>
2024-09-09 16:50:48 -0400

    commit

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,12 @@
 const express =require('express')
 const router = express.Router();
+
 router.route ('/').get((req, res)=>{
     res.send("all  items")
 
 })
+
+
 
 module.exports= router
 
--- server.js
+++ server.js
@@ -5,85 +5,40 @@ const morgan = require('morgan');
 const helper= require('./helper.js');
 const routes = require('./routes/tasks.js');
 let users = require('./routes/users');
+const bodyParser = require('body-parser')
 
 
-const errorHandler = (err, req, res, next) => {
-    console.error(err.user);
-    res.status(404).send('Something went wrong!');
-  };
+// Middleware to parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  const logging = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-};
-app.use(logging);
-app.use(express.json());
-
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
-app.get('/', function(req,res){
-    var obj = {
-         name:"Bousso",
-         age : "90 ans"}
-    res.render("index" , obj);
-})
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
 
-app.use('/users/id' ,(req, res)=>{
+// In-memory storage for messages
+let messages = [];
 
-    res.params.name(name)
-})
+// Handle GET request to render the form and display messages
+app.get('/', (req, res) => {
+  res.render('index', { messages: messages });
+});
 
+// Handle POST request to add a message
+app.post('/submit', (req, res) => {
+  const message = req.body.message;
+  messages.push(message);
+  res.redirect('/');
+});
 
+// Handle POST request to delete a message
+app.post('/delete', (req, res) => {
+  const messageIndex = req.body.index;
+  messages.splice(messageIndex, 1);
+  res.redirect('/');
+});
 
-
-
-app.use((req, res, next) => {
-    console.log(`URL: ${req.url}`)
-    next()
- })
-
- app.use ('/api/v1/routes' ,routes)
-
-
- app.use(morgan('dev'))
-
-
- app.listen(port, () => {
-
-  console.log("server star", port) 
-})
-// //routes
-//     app.get("/" ,( req, res) =>{
-//         res.send('ibrahima 2 ')
-//     } )
-
-   app.get('/users/:id', (req, res)=> {
-        const id = parseInt(req.params.id)
-       const user = users.find (user => user.id===id)
-       const message= "I found you "
-        res.json(helper.success(message,user))
-
-      res.send(`l'identite de l'utilisateur est le numero:  ${user.name}`)
-        
-     })
-
-
-    app.get('/users', (req, res)=> {
-        res.send(`the number of users is ${users.length } `)
-        
-     })
-
-    app.get('/users', (req, res)=>{ 
-        const message = 'the number of users is '
-        res.json(helper.success(message,users))
-    })
-    // app.post('/users',(req,res) =>{
-    //     const id = 11
-    //     const userNew = { ...req,body,...(id = id, created= new Date())}
-    // users.push(userNew)
-    // const message= `the user ${userNew.name} has been created`
-    // res.json(helper.success(message,use
-
-    // ))
-    // })
-    
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
